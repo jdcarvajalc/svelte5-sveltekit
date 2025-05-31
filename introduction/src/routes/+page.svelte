bc1742fc31506dc6189743fd3b5303062847409e
jdcarvajalc <jdcc1308@gmail.com>
2025-05-31 11:55:27 -0400

    feat: add simple button for understanding `onclick` event in svelte

--- introduction/src/routes/+page.svelte
+++ introduction/src/routes/+page.svelte
@@ -5,6 +5,21 @@
 
 <!-- Normal HTML -->
 <p>Hello {word}!</p>
+<button onclick={() => console.log('Button clicked')}>Click me</button>
 
 <!-- Normal css -->
-<style>p{color: green;}</style>
+<style>
+    p{color: green;}
+    button{
+         border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  background-color: #008CBA;
+    }
+</style>
