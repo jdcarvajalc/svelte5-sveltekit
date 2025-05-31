0f042a8f7d47da8b4b2dfda8dc67f1dd0d15b480
jdcarvajalc <jdcc1308@gmail.com>
2025-05-31 12:18:45 -0400

    refact: update the way to display info with `$derived.by()` rune

--- introduction/src/routes/+page.svelte
+++ introduction/src/routes/+page.svelte
@@ -2,13 +2,26 @@
 <script lang="ts">
     let word = $state<string>('world');
     let count = $state<number>(0);
+    
+    let info = $derived.by<string>(()=>{
+        if(count === 0){
+            return "Why don't you try clicking on the button";
+        }
+        if(count === 1){
+            return "You clicked the button once";
+        }
+        return "You clicked the button " + count + " times";
+    })
+
+    function onClick(){
+        count++;
+    }
 </script>
 
 <!-- Normal HTML -->
 <h2 class="text-green-700">Hello {word}!</h2>
-<p>Why don't you try clicking on the button</p>
-<button class="text-white my-1 mx-0.5 px-8 py-4 text-center bg-[#008CBA] cursor-pointer text-lg" onclick={() => {count++}}>
+<button class="text-white my-1 mx-0.5 px-8 py-4 text-center bg-[#008CBA] cursor-pointer text-lg" onclick={onClick}>
     Click me
 </button>
-<p>Button clicked {count} times</p>
+<p>{info}</p>
 
